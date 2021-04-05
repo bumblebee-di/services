d3eff60a385aa2ceaa4047608b7c64cf1c543e1c
Bumblebee Goat <bgoat@ge-n6.kzn.21-school.ru>
2021-04-05 16:33:42 +0300

    c

--- setup.sh
+++ setup.sh
@@ -19,20 +19,20 @@ eval $(minikube docker-env)
 docker build -t php_img srcs/php/
 kubectl apply -f srcs/php/phpmyadmin-service.yaml
 kubectl apply -f srcs/php/phpmyadmin-deployment.yaml
-eval $(minikube docker-env)
-docker build -t ftps_img srcs/ftps/
-kubectl apply -f srcs/ftps/ftps-service.yaml
-kubectl apply -f srcs/ftps/ftps-deployment.yaml
-eval $(minikube docker-env)
-docker build -t influxdb_img srcs/influxdb/
-kubectl apply -f srcs/influxdb/influxdb-pvc.yaml
-kubectl apply -f srcs/influxdb/influxdb-service.yaml
-kubectl apply -f srcs/influxdb/influxdb-deployment.yaml
-eval $(minikube docker-env)
-docker build -t grafana_img srcs/grafana/
-kubectl apply -f srcs/grafana/grafana-service.yaml
-kubectl apply -f srcs/grafana/grafana-deployment.yaml
-eval $(minikube docker-env)
-docker build -t telegraf_img srcs/telegraf/
-kubectl apply -f srcs/telegraf/telegraf-configmap.yaml
-kubectl apply -f srcs/telegraf/telegraf-deployment.yaml
+# eval $(minikube docker-env)
+# docker build -t ftps_img srcs/ftps/
+# kubectl apply -f srcs/ftps/ftps-service.yaml
+# kubectl apply -f srcs/ftps/ftps-deployment.yaml
+# eval $(minikube docker-env)
+# docker build -t influxdb_img srcs/influxdb/
+# kubectl apply -f srcs/influxdb/influxdb-pvc.yaml
+# kubectl apply -f srcs/influxdb/influxdb-service.yaml
+# kubectl apply -f srcs/influxdb/influxdb-deployment.yaml
+# eval $(minikube docker-env)
+# docker build -t grafana_img srcs/grafana/
+# kubectl apply -f srcs/grafana/grafana-service.yaml
+# kubectl apply -f srcs/grafana/grafana-deployment.yaml
+# eval $(minikube docker-env)
+# docker build -t telegraf_img srcs/telegraf/
+# kubectl apply -f srcs/telegraf/telegraf-configmap.yaml
+# kubectl apply -f srcs/telegraf/telegraf-deployment.yaml
